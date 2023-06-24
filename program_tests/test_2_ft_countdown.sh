690b8de5c062f409b71a5e918ec7b0375253ad87
egrfly <egrfly@outlook.com>
2023-06-24 14:19:28 +0100

    Add more subjects and tests

---
+++ program_tests/test_2_ft_countdown.sh
@@ -0,0 +1,1 @@
+./temporary_files/output_generator
---
+++ program_tests/test_4_aff_first_param.sh
@@ -0,0 +1,3 @@
+./temporary_files/output_generator vincent mit "l'ane" dans un pre et "s'en" vint
+./temporary_files/output_generator "j'aime le fromage de chevre"
+./temporary_files/output_generator
